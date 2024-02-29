d992782370744818b3843329ada6b55d68639276
ShaynaLeonard <shayna5762@gmail.com>
2024-02-29 16:42:38 +0200

    Initial commit

---
+++ server/routes/articles.js
@@ -0,0 +1,173 @@
+const fs = require('fs');
+const yup = require("yup");
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+
+
+
+
+
+// variables
+const dataPath = './server/data/articles.json';
+
+// helper methods
+const readFile = (callback, returnJson = false, filePath = dataPath, encoding = 'utf8') => {
+    fs.readFile(filePath, encoding, (err, data) => {
+        if (err) {
+            console.log(err);
+        }
+        if (!data) data = "{}";
+        callback(returnJson ? JSON.parse(data) : data);
+    });
+};
+
+const writeFile = (fileData, callback, filePath = dataPath, encoding = 'utf8') => {
+
+    fs.writeFile(filePath, fileData, encoding, (err) => {
+        if (err) {
+            console.log(err);
+        }
+
+        callback();
+    });
+};
+//CREATE
+const articleSchema = yup.object({
+    ID: yup.string().required(),
+    title: yup.string().required(),
+    dateOfPublished: yup.date().required(),
+    summary: yup.string().required(),
+    Author: yup.object({
+        nameAuthor: yup.string().required(),
+        authorEmail: yup.string().email().required(),
+        authorPhone: yup.string().required(),
+        authorHouseNum: yup.number().required(),
+    }),
+});
+
+// CreateArticle function
+const CreateArticle = async (req, res) => {
+    const ArticleDetails = req.body; // Extract payload from request body
+
+    try {
+        // Validate ArticleDetails against schema
+        await articleSchema.validate(ArticleDetails);
+
+        // Read data from file
+        readFile(data => {
+            // Add the new article
+            data[ArticleDetails.ID] = ArticleDetails;
+
+            // Write updated data to file
+            writeFile(JSON.stringify(data, null, 2), () => {
+                console.log('New article added successfully');
+                // Call the callback function with a success status
+                res.status(200).send(`article id:${ArticleDetails.ID} created`);
+            });
+        }, true);
+    } catch (error) {
+        console.error(error);
+
+        res.sendStatus(400);
+    }
+};
+
+//UPDATE 
+const articleUpdateSchema = yup.object({
+    title: yup.string(),
+    dateOfPublished: yup.date(),
+    summary: yup.string(),
+});
+
+const updateArticle = async (req,res) =>{
+    const articleId = req.params.id; //needs to be string!
+    const detailToUpdate = req.body; 
+    await articleUpdateSchema.validate(detailToUpdate);
+
+    try{
+        readFile(data => {
+            if (data[articleId]){
+                console.log("found the article")
+                if(detailToUpdate.title){
+                    data[articleId].title = detailToUpdate.title; 
+                    console.log("has title")
+                }
+                if(detailToUpdate.dateOfPublished){
+                    data[articleId].dateOfPublished = detailToUpdate.dateOfPublished; 
+                    console.log("has date published")
+                }
+                if(detailToUpdate.summary){
+                    data[articleId].summary = detailToUpdate.summary; 
+                    console.log("has summary")
+                }
+            }
+            writeFile(JSON.stringify(data, null, 2), () => {
+                console.log('Updated article successfully');
+                // Call the callback function with a success status
+                res.status(200).send(`article id:${articleId} updated`);
+            });
+
+        }, true); 
+    } catch (error) {
+        console.error(error);
+        res.sendStatus(400);
+    }
+}; 
+
+
+module.exports = {
+    CreateArticle,
+    articleSchema,
+    articleUpdateSchema, 
+    updateArticle, 
+
+    //READ
+    getArticles: function (req, res) {
+        fs.readFile(dataPath, 'utf8', (err, data) => {
+            if (err) {
+                console.log(err);
+                res.sendStatus(500);
+            }
+            else
+                res.send(!data ? JSON.parse("{}") : JSON.parse(data));
+        });
+    },
+
+
+    // UPDATE
+    update_user: function (req, res) {
+
+        readFile(data => {
+
+            // add the new user
+            const userId = req.params["id"];
+            if (data[userId])
+                data[userId] = req.body;
+            else res.sendStatus(400);
+
+            console.log("after if")
+
+            writeFile(JSON.stringify(data, null, 2), () => {
+                res.status(200).send(`users id:${userId} updated`);
+            });
+        },
+            true);
+    },
+    // DELETE
+    delete_user: function (req, res) {
+
+        readFile(data => {
+
+            // add the new user
+            const userId = req.params["id"];
+            delete data[userId];
+
+            writeFile(JSON.stringify(data, null, 2), () => {
+                res.status(200).send(`users id:${userId} removed`);
+            });
+        },
+            true);
+    }
+};
---
+++ server/routes/routes.js
@@ -0,0 +1,18 @@
+const express = require('express'),
+    articlesRoutes = require('./articles');
+
+var router = express.Router();
+
+//articles
+router.post('/articles', articlesRoutes.CreateArticle);
+// router.put('/articles/:id', articlesRoutes.updateArticle);
+router.put('/articles/:id', articlesRoutes.updateArticle); // http://localhost:3001/articles/123
+// router.post('/articles/:id/images', articlesRoutes.AddImagesToArticle);
+// router.get('/articles/:id', articlesRoutes.getArticles);
+router.get('/articles', articlesRoutes.getArticles);
+// router.delete('/articles/:id/images/:url', articlesRoutes.deleteImageFromArticle);
+// router.delete('/articles/:id', articlesRoutes.deleteArticles);
+
+//
+
+module.exports = router;
